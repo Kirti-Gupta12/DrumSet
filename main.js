89c72bbd8cf2bd5afef32afd29f07dd5377cf6b3
Kirti-Gupta12 <87939523+Kirti-Gupta12@users.noreply.github.com>
2022-07-26 23:10:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// console.log('JS Loaded');
+
+function removeTransition(e) {
+
+    if (e.propertyName !== 'transform') return;
+    
+    e.target.classList.remove('playing');
+    
+    }
+
+    function playSound(e) {
+
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+        if(!audio) return;
+
+        key.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    const keys = Array.from(document.querySelectorAll('.sub-contain'));
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
